de58a8889a22a348fb3481ba16ee87699b06cbc8
Kevin <kevinolsson305@gmail.com>
2020-03-28 00:14:53 +0100

    bugtesting

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
 import { DataProvider } from './DataContext'
 import { Head } from 'components/Head/Head'; 
 import { Router } from 'Router';
 import { theme } from 'Theme';
 import data from './data.json'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    opacity: '1 !important'
-  }
-}), { name: 'app' });
-
 const App = () => {
-  const [hasMounted, setHasMounted] = React.useState(false);
-  const classes = useStyles();
-
-  React.useEffect(() => {
-    setHasMounted(true)
-  }, [])
-
   return (
-    <div id="app" className={hasMounted ? classes.root : undefined}>
       <MuiThemeProvider theme={theme}>
         <DataProvider value={data}>
           <Head />
           <Router />
         </DataProvider>
       </MuiThemeProvider>
-    </div>
   );
 };
 

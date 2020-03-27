2fcfb79157118414b9b6809aecf9d28830fa18d3
Kevin <kevinolsson305@gmail.com>
2020-03-28 00:42:23 +0100

    removed tornis usage see if that does the job

--- src/components/HamburgerButton/contextAwareHamburgerButton.js
+++ src/components/HamburgerButton/contextAwareHamburgerButton.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
-import { watchViewport, unwatchViewport } from 'tornis';
 
 export const contextAwareHamburgerButton = (Component) => ({ active, handleClickCallback }) => {
-  const [hasScrolled, setHasScrolled] = React.useState(false);
   const history = useHistory();
   const back = history.location.key ? history.goBack : '/';
 
   const pathname = history.location.pathname.split('/');
   const isNestedPage = !!pathname[2];
 
-  const handleScroll = ({ scroll }) => {
-    if (scroll.changed) {
-      setHasScrolled(scroll.top > 164);
-    }
-  };
 
-  React.useEffect(() => {
-    watchViewport(handleScroll);
-    return () => {
-      unwatchViewport(handleScroll);
-    };
-  }, []);
 
   return (
     <Component
-      showBack={isNestedPage && !active && hasScrolled}
+      showBack={isNestedPage && !active}
       back={back}
       handleClickCallback={handleClickCallback}
       active={active}

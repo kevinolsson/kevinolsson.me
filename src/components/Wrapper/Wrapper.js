ff4781345c7f62aa1e6c7fa73e6bb1a80f8ded58
Kevin <kevinolsson305@gmail.com>
2020-03-28 00:19:12 +0100

    hello

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Layout } from 'components/Layout/Layout';
 import { Header } from 'components/Header/Header';
-import { MobileNavigation } from 'components/MobileNavigation/MobileNavigation';
 
 export const Wrapper = (props) => {
   const { children } = props;
   return (
     <>
-      <MobileNavigation />
       <Layout>
         {<Header />}
         {children}

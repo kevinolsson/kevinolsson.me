48baeab01764f803860b7f5cea7bdd109ed726c5
Kevin <kevinolsson305@gmail.com>
2020-03-27 23:45:12 +0100

    mobile nav and cheap css trick to hide loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { DataProvider } from './DataContext'
 import { Head } from 'components/Head/Head'; 
 import { Router } from 'Router';
 import { theme } from 'Theme';
 import data from './data.json'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    opacity: '1 !important'
+  }
+}), { name: 'app' });
+
 const App = () => {
-  console.log('data: ',data)
+  const classes = useStyles();
   return (
-    <MuiThemeProvider theme={theme}>
-      <DataProvider value={data}>
-        <Head />
-        <Router />
-      </DataProvider>
-    </MuiThemeProvider>
+    <div id="app" className={classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <DataProvider value={data}>
+          <Head />
+          <Router />
+        </DataProvider>
+      </MuiThemeProvider>
+    </div>
   );
 };
 
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Layout } from 'components/Layout/Layout';
 import { Header } from 'components/Header/Header';
+import { MobileNavigation } from 'components/MobileNavigation/MobileNavigation';
 
 export const Wrapper = (props) => {
   const { children } = props;
   return (
     <>
-      {/* <MobileNavigation /> */}
+      <MobileNavigation />
       <Layout>
         {<Header />}
         {children}

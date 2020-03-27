dc11d413c6d7acfe20aba6cbab24f0b8c1d77313
Kevin <kevinolsson305@gmail.com>
2020-03-28 00:50:17 +0100

    removed torrnis

--- src/components/HamburgerButton/contextAwareHamburgerButton.js
+++ src/components/HamburgerButton/contextAwareHamburgerButton.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 export const contextAwareHamburgerButton = (Component) => ({ active, handleClickCallback }) => {
+  const trigger = useScrollTrigger({
+    threshold: 164
+  });
+
   const history = useHistory();
   const back = history.location.key ? history.goBack : '/';
-
   const pathname = history.location.pathname.split('/');
   const isNestedPage = !!pathname[2];
 
-
-
   return (
     <Component
-      showBack={isNestedPage && !active}
+      showBack={isNestedPage && !active && trigger}
       back={back}
       handleClickCallback={handleClickCallback}
       active={active}

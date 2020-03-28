5062c7c9b99dd9a4a087219a8b980c6fe1bbd75f
Kevin <kevinolsson305@gmail.com>
2020-03-28 01:32:32 +0100

    checking if its the scroll

--- src/components/HamburgerButton/contextAwareHamburgerButton.js
+++ src/components/HamburgerButton/contextAwareHamburgerButton.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
 
 export const contextAwareHamburgerButton = (Component) => ({ active, handleClickCallback }) => {
-  const trigger = useScrollTrigger({
-    threshold: 164
-  });
-
   const history = useHistory();
   const back = history.location.key ? history.goBack : '/';
   const pathname = history.location.pathname.split('/');
@@ -15,7 +11,7 @@ export const contextAwareHamburgerButton = (Component) => ({ active, handleClick
 
   return (
     <Component
-      showBack={isNestedPage && !active && trigger}
+      showBack={isNestedPage && !active}
       back={back}
       handleClickCallback={handleClickCallback}
       active={active}

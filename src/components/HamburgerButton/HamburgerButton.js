e6833964937b59d28620cf912c53c11d1994d7b8
Kevin <kevinolsson305@gmail.com>
2020-03-28 01:12:20 +0100

    styligns here are messed up

--- src/components/HamburgerButton/HamburgerButton.js
+++ src/components/HamburgerButton/HamburgerButton.js
@@ -2,59 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { HamburgerIcon } from 'components/HamburgerIcon/HamburgerIcon';
-
-const useStyles = makeStyles((theme) => ({
-  root: ({ forceMobileView }) => ({
-    position: 'fixed',
-    zIndex: 150,
-    bottom: theme.spacing(4),
-    right: theme.spacing(4),
-    [theme.breakpoints.up('md')]: {
-      display: !forceMobileView && 'none',
-    },
-  }),
-  button: {
-    width: theme.spacing(16),
-    height: theme.spacing(16),
-    border: 'none',
-    outline: 'none',
-    borderRadius: '100%',
-    backgroundColor: theme.palette.portfolio.green,
-    position: 'relative',
-    zIndex: 10,
-    padding: 0,
-  },
-  buttonActive: {
-    backgroundColor: theme.palette.portfolio.grey,
-  },
-  buttonBack: {
-    position: 'absolute',
-    transition: '0.1s ease-out right',
-    width: theme.spacing(12),
-    height: theme.spacing(12),
-    backgroundColor: theme.palette.portfolio.grey,
-    top: theme.spacing(2),
-    right: 0,
-    zIndex: 5,
-  },
-  buttonBackActive: {
-    right: theme.spacing(14),
-  },
-  backIcon: {
-    margin: theme.spacing(3),
-    fill: theme.palette.common.white,
-  },
-}), { name: 'hamburgerButton' });
 
 
 export const HamburgerButton = ({
   handleClickCallback, active, forceMobileView, back, showBack,
 }) => {
   const [backActive, setBackActive] = React.useState(false);
-  const classes = useStyles({ forceMobileView });
 
   const handleClick = () => {
     if (handleClickCallback) {
@@ -69,7 +24,7 @@ export const HamburgerButton = ({
   }, [back, showBack]);
 
   return (
-    <div className={classes.root}>
+    <div>
       <button
         type="button"
         label="menu"
@@ -91,7 +46,7 @@ export const HamburgerButton = ({
           //   backActive && classes.buttonBackActive,
           // ].filter(Boolean).join(' ')}
         >
-          <ArrowBackIcon className={classes.backIcon} />
+          <ArrowBackIcon />
         </Component>
       )}
     </div>

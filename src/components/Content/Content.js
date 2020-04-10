68af2b64044b0893b300f75dcb6ee806b0c3dc40
Kevin Olsson <kevin@pamind.se>
2020-04-10 21:56:17 +0200

    links

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -21,13 +21,15 @@ const useStyles = makeStyles(
       '& img': {
         width: '100%'
       },
-      '& a': { color: theme.palette.portfolio.primary },
+      '& a': { color: theme.palette.portfolio.pink },
       '& h1': { ...theme.typography.h1 },
       '& h2': { ...theme.typography.h2 },
       '& h3': { ...theme.typography.h3 },
       '& h4': { ...theme.typography.h4 },
       '& h5': { ...theme.typography.h5 },
-      '& p': { ...theme.typography.body1 }
+      '& p, & ul': {
+        ...theme.typography.body1
+      }
     }
   }),
   { name: 'Content' }

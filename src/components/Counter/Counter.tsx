660175f520bafb45f4a6b3cd47ba9b9091e42300
Kevin <kevinolsson305@gmail.com>
2021-10-24 13:37:16 +0200

    remove .scss files

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment, incrementByAmount } from "state/actions";
-import styles from "./Counter.module.scss";
+import styles from "./Counter.module.css";
 
 interface RootState {
   counter: {

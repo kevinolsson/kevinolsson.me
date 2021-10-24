83983e2b1175ec6c9641c829f2d7e5d7965c87ac
Kevin <kevinolsson305@gmail.com>
2021-10-24 02:21:06 +0200

    ugh git

--- src/DataContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DataContext = React.createContext(true);
-export const DataProvider = DataContext.Provider;
-export const DataConsumer = DataContext.Consumer;
-
-export default DataContext;
--- src/components/ProjectPost/ProjectPost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Content from 'components/Content/Content';
-
-export const ProjectPost = ({ body }) => <Content src={body} />;
-
-ProjectPost.propTypes = {
-  body: PropTypes.string
-};

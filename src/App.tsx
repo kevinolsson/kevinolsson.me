2418319c0ce58db4e576ee0e75959045a64ef6bf
Kevin <kevinolsson305@gmail.com>
2022-06-16 00:35:22 +0200

    basic theme handling using css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { Router } from "Router";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "components/ThemeProvider/ThemeProvider";
+import React from 'react';
+import { Router } from 'Router';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'components/ThemeProvider/ThemeProvider';
 
 type AppProps = { store: any };
 export const App = ({ store }: AppProps): JSX.Element => {
-  const [hasMounted, setHasMounted] = React.useState(false);
+    const [hasMounted, setHasMounted] = React.useState(false);
 
-  React.useEffect(() => {
-    setHasMounted(true);
-  }, []);
+    React.useEffect(() => {
+        setHasMounted(true);
+    }, []);
 
-  return (
-    <div id="app" style={hasMounted ? { opacity: 1 } : undefined}>
-      <Provider store={store}>
-        <ThemeProvider>
-          <Router />
-        </ThemeProvider>
-      </Provider>
-    </div>
-  );
+    return (
+        <div id="app" style={hasMounted ? { opacity: 1 } : undefined}>
+            <Provider store={store}>
+                <ThemeProvider>
+                    <Router />
+                </ThemeProvider>
+            </Provider>
+        </div>
+    );
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import makeStyles from "@mui/styles/makeStyles";
+import React from 'react';
+import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = makeStyles(
-  (theme?: any) => ({
-    root: {
-      boxSizing: "border-box",
-      maxWidth: "640px",
-      margin: `${theme.spacing(4)} auto`,
-      padding: theme.spacing(4),
-      paddingBottom: theme.spacing(24),
-      transition: "0.2s ease-out all",
-      [theme.breakpoints.up("md")]: {
-        maxWidth: "1024px"
-      }
-    }
-  }),
-  { name: "layout" }
+    (theme?: any) => ({
+        root: {
+            boxSizing: 'border-box',
+            maxWidth: '640px',
+            margin: `${theme.spacing(4)} auto`,
+            padding: theme.spacing(4),
+            paddingBottom: theme.spacing(24),
+            transition: '0.2s ease-out all',
+            [theme.breakpoints.up('md')]: {
+                maxWidth: '1024px',
+            },
+        },
+    }),
+    { name: 'layout' }
 );
 
 interface ILayout {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const Layout = ({ children }: ILayout): JSX.Element => {
-  const classes = useStyles();
-  return <div className={classes.root}>{children}</div>;
+    const classes = useStyles();
+    return <div className={classes.root}>{children}</div>;
 };
